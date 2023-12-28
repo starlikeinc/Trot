a3dec5490febd4c830e6073829f06e66c8b65bba
flashe0404Starlike <flashe0404@starlike.kr>
2023-12-28 14:56:40 +0900

    TTScenePatcher 업데이트
    현재 번들 다운로드 url이 없기 때문에 TTSceneStepPatcher에서 Fake Download 구현 및 추후 url 생성 시 실제 다운로드가 가능하도록 함수 구현 및 해당 함수 실행 부분 주석처리.
    
    추가로 Resources/FrontImage에 다운로드바 더미 이미지 추가, 추후 제거 필요.

--- SLProjectTrotUnity/Assets/03.CodeContents/TTCodeResource/TTManagerPatcher.cs
+++ SLProjectTrotUnity/Assets/03.CodeContents/TTCodeResource/TTManagerPatcher.cs
@@ -1,14 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TTManagerPatcher : CManagerPatchAssetBundleBase
-{	public static new TTManagerPatcher Instance { get { return CManagerPatchAssetBundleBase.Instance as TTManagerPatcher; } }
-	//------------------------------------------------------------------------
-	[SerializeField]
-	private string MainLabelName = "Main";
+{
+    public static new TTManagerPatcher Instance { get { return CManagerPatchAssetBundleBase.Instance as TTManagerPatcher; } }
+    //------------------------------------------------------------------------
+    [SerializeField]
+    private string MainLabelName = "Main";
 
-	//-------------------------------------------------------------------------
+    private List<string> m_listAssetBundleLabels = new List<string>();
 
+    private long downLoadSize;
+    //-------------------------------------------------------------------------
+
+    public CPatcherBase.SPatchEvent DoPatcherInitialize(bool bResetEventHandler, string strDownloadURL)
+    {
+        return ProtPatchAssetBundleInitialize(bResetEventHandler, strDownloadURL);
+    }
+
+    public void DoPatcherStart()
+    {
+        ProtPatchAddressableStart(m_listAssetBundleLabels);
+    }
+
+    public void DoPatcherTotalDownloadSize(UnityAction<long> delFinish)
+    {
+        ProtPatchAddressableTotalDowloadSize(m_listAssetBundleLabels, PrivGetDownloadSize);
+        delFinish(PrivByteToMB(downLoadSize));
+    }
+    //-------------------------------------------------------------------------
+
+    private long PrivByteToMB(long byteSize)
+    {
+        return (long)((byteSize / (double)Math.Pow(1024, 2)));
+    }
+
+    private void PrivGetDownloadSize(long size)
+    {
+        downLoadSize = size;
+    }
+    //-------------------------------------------------------------------------
+
+    protected override void OnPatcherInitComplete()
+    {
+
+    }
+
+    protected override void OnPatcherDownloadSize(string strLabelhName, long iSize)
+    {
+
+    }
+
+    protected override void OnPatcherProgress(string strLabelhName, long iDownloadedByte, long iTotalByte, float fProgressPercent, uint iLoadCurrent, uint iLoadMax)
+    {
+
+    }
+
+    protected override void OnPatcherEnd()
+    {
+
+    }
+
+    protected override void OnPatcherStartLabel(string strLabelName)
+    {
+
+    }
+
+    protected override void OnPatcherError(CPatcherBase.EPatchError eErrorType, string strMessage)
+    {
+
+    }
 
 }
--- SLProjectTrotUnity/Assets/03.CodeContents/TTCodeScene/TTCodeSceneStep/TTSceneStepPatcher.cs
+++ SLProjectTrotUnity/Assets/03.CodeContents/TTCodeScene/TTCodeSceneStep/TTSceneStepPatcher.cs
@@ -2,20 +2,142 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class TTSceneStepPatcher : MonoBehaviour
 {
-    void Start()
+    [Header("DownloadStart")]
+    [SerializeField] private CImage ImgBGDownload;
+    [SerializeField] private CText TxtDownloadSize;
+
+    [Header("DownloadProgress")]
+    [SerializeField] private CImage ImgBGProgress;
+    [SerializeField] private CImage ImgProgressBar;
+    [SerializeField] private CText TxtDownloadProgress;
+
+    [Header("DownloadError")]
+    [SerializeField] private CImage ImgBGError;
+    [SerializeField] private CText TxtErrorDescrip;
+
+
+    private CPatcherBase.SPatchEvent mPatchEvent;
+
+    private long mDownloadSize;
+    //------------------------------------------------------------------------
+    private void Start()
     {
-		Invoke("HandleStepPatcherFinish", 1f);
-	}
+        mPatchEvent = TTManagerPatcher.Instance.DoPatcherInitialize(false, TTManagerPatcher.DownloadURL);
+        mPatchEvent.EventPatchProgress += OnPatcherProgress;
+        mPatchEvent.EventPatchFinish += OnPatcherEnd;
+        mPatchEvent.EventPatchError += OnPatcherError;
 
+        TTManagerPatcher.Instance.DoPatcherTotalDownloadSize(PrivShowDownloadSize);
+    }
 
-	//-------------------------------------------------------------
-	private void HandleStepPatcherFinish()
-	{
-		TTManagerSceneLoader.Instance.DoMgrSceneLoaderGoToMaster(null);
+    //------------------------------------------------------------------------
 
+    private void OnPatcherProgress(string Name, long _downloadedByte, long _totalByte, float Progress, uint _loadCurrent, uint _loadMax)
+    {
+        ImgProgressBar.fillAmount = Progress;
+        TxtDownloadProgress.text = $"( {_downloadedByte} / {mDownloadSize} ) MB {Progress * 100}%";
+    }
+    private void OnPatcherEnd()
+    {
+        ImgProgressBar.fillAmount = 1;
+        TxtDownloadProgress.text = "100 %";
 
-	}
+        TTManagerSceneLoader.Instance.DoMgrSceneLoaderGoToMaster(null);
+    }
+    private void OnPatcherError(CPatcherBase.EPatchError errorType, string message)
+    {
+        TxtErrorDescrip.text = $"{errorType}\n{message}";
+        ImgBGError.gameObject.SetActive(true);
+    }
+    //------------------------------------------------------------------------
+
+    private void PrivShowDownloadSize(long size)
+    {
+        PrivShowFakeDownloadSize();
+        //PrivShowRealDownloadSize(size);
+    }
+    private void PrivStartDownload()
+    {
+        ImgBGDownload.gameObject.SetActive(false);
+        ImgBGProgress.gameObject.SetActive(true);
+
+        PrivStartFakeDownload();
+        //PrivStartRealDownload();
+    }
+    private void PrivCancelDownload()
+    {
+        Debug.Log("CancelDownload");
+    }
+    private void PrivConfirmError()
+    {
+        Debug.Log("ConfirmError");
+    }
+
+    private void PrivShowRealDownloadSize(long size)
+    {
+        mDownloadSize = size;
+        TxtDownloadSize.text = $"( {size.ToString("0")}MB )";
+    }
+    private void PrivStartRealDownload()
+    {
+        TTManagerPatcher.Instance.DoPatcherStart();
+    }
+
+    #region ===== FakeDownload =====
+
+    private int FakeDownloadSize = 39;
+    private float FakeDownloadTime = 2;
+
+    private void PrivShowFakeDownloadSize()
+    {
+        TxtDownloadSize.text = $"( {FakeDownloadSize}MB )";
+    }
+
+    private void PrivStartFakeDownload()
+    {
+        StartCoroutine(PrivCOFakeLoad());
+    }
+
+    private IEnumerator PrivCOFakeLoad()
+    {
+        float progressTime = 0;
+
+        while(progressTime < FakeDownloadTime)
+        {
+            float progress = progressTime / FakeDownloadTime;
+            int downloadedMB = (int)(progress * FakeDownloadSize);
+
+            ImgProgressBar.fillAmount = progress;
+            TxtDownloadProgress.text = $"( {downloadedMB} / {FakeDownloadSize} ) MB {(progress * 100).ToString("0.0")}%";
+
+            progressTime += Time.deltaTime;
+            yield return null;
+        }
+
+        ImgProgressBar.fillAmount = 1;
+        TxtDownloadProgress.text = $"( {FakeDownloadSize} / {FakeDownloadSize} ) MB 100%";
+
+        TTManagerSceneLoader.Instance.DoMgrSceneLoaderGoToMaster(null);
+    }
+
+    #endregion
+
+    //------------------------------------------------------------------------
+
+    public void HandleOnClickButtonDownloadYes()
+    {
+        PrivStartDownload();
+    }
+    public void HandleOnClickButtonDownloadNo()
+    {
+        PrivCancelDownload();
+    }
+    public void HandleOnClickButtonErrorConfirm()
+    {
+        PrivConfirmError();
+    }
 }
